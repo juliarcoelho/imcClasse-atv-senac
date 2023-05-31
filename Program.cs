897bd72e43dab9abf08084f92aee71368bf87fe2
juliarcoelho <juliarochx@gmail.com>
2023-05-31 16:02:30 -0300

    adicionando readme

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace imcClasse
+{
+    class Imc
+    {
+        private string nome;
+        private double peso, altura;
+
+        public string nomePublico
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+
+        public double pesoPublico
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public double alturaPublico
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+        public Imc() { }
+
+        public double CalculaImc()
+        {
+            double imc;
+
+            imc = peso / (altura * altura);
+            return imc;
+        }
+        public void MostraImc()
+        {
+            Console.WriteLine(nome + " o seu IMC é: " + CalculaImc());
+        }
+        public void MostraSituacao(double imc)
+        {
+            if (imc < 20)
+            {
+                Console.WriteLine("Abaixo do peso.");
+            }
+            else
+            {
+                Console.WriteLine("Peso normal.");
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Imc paciente = new Imc();
+
+            Console.WriteLine("Digite o nome: ");
+            paciente.nomePublico = Console.ReadLine();
+
+            Console.WriteLine("Digite o peso: ");
+            paciente.pesoPublico = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite a altura: ");
+            paciente.alturaPublico = Convert.ToDouble(Console.ReadLine());
+
+            paciente.MostraImc();
+            paciente.MostraSituacao(paciente.CalculaImc());
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Estudo da Utilização de Classe em C#: Cálculo de IMC
+
+Este repositório contém um estudo da utilização de classes em C#, realizado como parte do curso técnico no Senac. O código solicita dados do usuário, como nome, peso e altura, calcula o IMC e retorna se a pessoa está abaixo, dentro ou acima do peso. São utilizadas variáveis públicas (`public`) e privadas (`private`) para armazenar os dados.
+
+## Funcionalidades
+
+- Solicita ao usuário que insira seus dados pessoais, incluindo nome, peso e altura.
+- Calcula o IMC com base no peso e altura fornecidos.
+- Classifica o estado de peso do usuário com base no IMC calculado, retornando se a pessoa está abaixo, dentro ou acima do peso.
+
+## Pré-requisitos
+
+- Ambiente de desenvolvimento compatível com C# 
+- .NET Framework (ou versão superior)
+
